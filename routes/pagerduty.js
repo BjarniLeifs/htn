8aec31e1dd1bb8bc0a28dedb8713116795402f0e
Alan Ma <alan_ma@live.ca>
2017-09-17 03:57:09 -0400

    fixed delimiter

--- routes/pagerduty.js
+++ routes/pagerduty.js
@@ -32,7 +32,7 @@ router.post('/incident', function(req, res, next) {
       form: {
         'incident': {
           'type': 'incident',
-          'title': req.body.title + " ||DELIMTER|| " + req.body.image,
+          'title': req.body.title + " ||DELIMITER|| " + req.body.image,
           'service': {
             'id': 'PS337ZU', //token.pagerdutyid,
             'type': 'service_reference'
