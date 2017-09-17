643b1d7699eca93230ddb72f15ab6c34ca77215f
Bjarni Leifsson <bjarnil10@ru.is>
2017-09-17 04:09:42 -0400

    updating jwtcheck and closing routes for only logged in

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+const jwtCheck = require('express-jwt');
 /* Sockets */
 var io = require('socket.io')(http);
 
